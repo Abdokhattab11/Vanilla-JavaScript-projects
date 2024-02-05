9ecef1e7da220f6b139a1341dec5a72df58803d2
AbdoKhattab11 <mr.spykhattab@gmail.com>
2024-02-06 00:43:41 +0200

    Added logic JS

--- 8- Meal Finder Project/meal.js
+++ 8- Meal Finder Project/meal.js
@@ -5,7 +5,6 @@ export default class Meal {
     this.area = data["strArea"];
     this.instructions = data["strInstructions"];
     this.img = data["strMealThumb"];
-    this.tags = data["strTags"] ? data["strTags"].split(/,/) : null;
     this.ingredients = [];
     for (let i = 1; i <= 20; i++) {
       let strIng = `strIngredient${i}`;
@@ -15,5 +14,50 @@ export default class Meal {
       ans = `${data[strIng]} - ${data[strMeasure]}`;
       this.ingredients.push(ans);
     }
+  }
+  renderGallary() {
+    return `
+      <p class="meal-title">${this.name}</p>
+      <div>
+        <img
+          src="${this.img}"
+          alt="${this.name}" />
+      </div>
+      `;
+  }
+  rederAllInfo() {
+    return `
+    <div class="specific-meal-cnt">
+      <h2>${this.name}</h2>
+      <div class="img">
+        <img
+          src="${this.img}"
+          alt="${this.name}" />
+      </div>
+      <div class="meal-type">
+        <p>${this.category}</p>
+        <p>${this.area}</p>
+      </div>
+      <div class="meal-info">
+        <p>
+          ${this.instructions}
+        </p>
+      </div>
+      <div class="meal-ingerdients">
+        <h3>Ingredients</h3>
+        <ul class="ingerdients-list">  
+          ${this.getAllIngrediant()}        
+        </ul>
+      </div>
+    </div>`;
+  }
+  getAllIngrediant() {
+    let html = ``;
+    for (let i = 0; i < this.ingredients.length; i++) {
+      html += `<li class="ingerdients-item">
+        <p>${this.ingredients[i]}</p>
+      </li>`;
+    }
+    return html;
   }
 }
--- 8- Meal Finder Project/script.js
+++ 8- Meal Finder Project/script.js
@@ -3,17 +3,71 @@ import Meal from "./meal.js";
 const input = document.getElementById("meal-input");
 const seachBtn = document.getElementById("search-btn");
 const suffleBtn = document.getElementById("suffle-btn");
+const mealAllInfo = document.querySelector(".specific-meal-info");
+const mealsSection = document.querySelector(".meals-section");
 
 const randomMealUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
-const mealsByNameUrl = "www.themealdb.com/api/json/v1/1/search.php?s="; // name
+const mealsByNameUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s="; // name
+
+function getMealData(mealJson, idx = 0) {
+  return mealJson["meals"][idx];
+}
+
+function displayAllMealInfo(meal) {
+  if (mealAllInfo.classList.contains("hidden"))
+    mealAllInfo.classList.remove("hidden");
+  mealAllInfo.innerHTML = meal.rederAllInfo();
+}
 
 async function getRandomMeal() {
   const response = await fetch(randomMealUrl);
   const data = await response.json();
-  console.log(data["meals"][0]);
-  const meal = new Meal(data["meals"][0]);
+  const meal = new Meal(getMealData(data));
+  if (mealAllInfo.classList.contains("hidden"))
+    mealAllInfo.classList.remove("hidden");
+  mealAllInfo.innerHTML = meal.rederAllInfo();
   console.log(meal);
 }
-async function getMeals() {}
 
-getRandomMeal();
+async function getMealsByName(name) {
+  if (mealsSection.classList.contains("hidden"))
+    mealsSection.classList.remove("hidden");
+  if (!mealAllInfo.classList.contains("hidden"))
+    mealAllInfo.classList.add("hidden");
+  const mealsList = mealsSection.querySelector("ul");
+  const mealsTitle = mealsSection.querySelector("h3");
+  const url = `${mealsByNameUrl}${name}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    mealsList.innerHTML = ``;
+    mealsTitle.innerText = ``;
+    for (let i = 0; i < data["meals"].length; i++) {
+      const meal = new Meal(getMealData(data, i));
+      const mealDom = document.createElement("li");
+      mealDom.className = "meal-item";
+      mealDom.innerHTML = meal.renderGallary();
+      mealsList.append(mealDom);
+      mealDom.addEventListener("click", displayAllMealInfo.bind(null, meal));
+    }
+  } catch (e) {
+    mealsTitle.innerText = `Can't Find Meals With Name ${name}`;
+    return;
+  }
+}
+
+suffleBtn.addEventListener("click", (e) => {
+  // To prevent page from reloading
+  // Each Time we click on suffle
+  if (!mealsSection.classList.contains("hidden"))
+    mealsSection.classList.add("hidden");
+  e.preventDefault();
+  getRandomMeal();
+});
+seachBtn.addEventListener("click", (e) => {
+  // To prevent page from reloading
+  // Each Time we click on Search
+  e.preventDefault();
+  const mealName = input.value;
+  getMealsByName(mealName);
+});
